4729ff891ab8147db6273fe59c887f3904cec6a1
jeksonvpurba <50542915+jeksonvpurba@users.noreply.github.com>
2019-05-26 21:46:00 +0800

    Update SD_20182_T2_E1E117033.java

--- SD_20182_T2_E1E117033.java
+++ SD_20182_T2_E1E117033.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class tugas2 {
+public class SD_20182_T2_E1E117033 {
     public static void main(String[] args) {
         
         Scanner input=new Scanner(System.in);
